ed60b7d07c191fc9ef661d5a6fd41275e6127b30
Olley <olga.e.timofeeva@gmail.com>
2023-03-02 01:35:45 +0000

    adds chnages for Rollbar instrumentation

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -26,6 +26,11 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
+# import rollbar libraries
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 #import libraries for CloudWatch
 import watchtower
 import logging
@@ -70,12 +75,34 @@ cors = CORS(
   methods="OPTIONS,GET,HEAD,POST"
 )
 
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
 @app.after_request
 def after_request(response):
     timestamp = strftime('[%Y-%b-%d %H:%M]')
     LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
     return response
 
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
+    
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
   user_handle  = 'andrewbrown'
