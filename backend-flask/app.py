74acdc9ee39e19857ddb9f9e66abefce9139f5cc
Olley <olga.e.timofeeva@gmail.com>
2023-03-15 23:07:52 +0000

    instruments JWT token verification with custom library

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -14,6 +14,9 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+# import Cruddur custom libraries
+from lib.cognito_jwt_token import CognitoJwtToken, extract_access_token, TokenVerifyError
+
 # import libraries for HoneyComb 
 from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
@@ -67,12 +70,20 @@ RequestsInstrumentor().instrument()
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
+
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
   headers=['Content-Type', 'Authorization'], 
   expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
+)
+
+# creates a JWT token
+cognito_jwt_token = CognitoJwtToken(
+  user_pool_id=os.getenv("AWS_COGNITO_USER_POOL_ID"), 
+  user_pool_client_id=os.getenv("AWS_COGNITO_USER_POOL_CLIENT_ID"),
+  region=os.getenv("AWS_DEFAULT_REGION")
 )
 
 rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
@@ -142,9 +153,22 @@ def data_create_message():
 @xray_recorder.capture('home_trace')
 def data_home():
   app.logger.debug('AUTH HEADER')
-  app.logger.debug(request.headers.get('Authorization')
+  app.logger.debug(request.headers.get('Authorization'))
   
   data = HomeActivities.run(logger=LOGGER)
+  access_token = extract_access_token(request.headers)
+  try:
+    claims = cognito_jwt_token.verify(access_token)
+    # authenicatied request
+    app.logger.debug("authenicated")
+    app.logger.debug(claims)
+    app.logger.debug(claims['username'])
+    data = HomeActivities.run(cognito_user_id=claims['username'])
+  except TokenVerifyError as e:
+    # unauthenicatied request
+    app.logger.debug(e)
+    app.logger.debug("unauthenicated")
+    data = HomeActivities.run()
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
