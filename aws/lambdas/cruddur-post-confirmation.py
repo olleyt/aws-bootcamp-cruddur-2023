16ad1efb02732b338856614da702c10e6a74ecd9
Olley <32430092+olleyt@users.noreply.github.com>
2023-03-24 19:27:13 +1100

    starts refactoring of lambda handler
    Note: this version is not for copying into AWS Lambda
    I am only transitioning it from GitHub while GitPod starts

--- aws/lambdas/cruddur-post-confirmation.py
+++ aws/lambdas/cruddur-post-confirmation.py
@@ -3,15 +3,22 @@ import psycopg2
 
 def lambda_handler(event, context):
     user = event['request']['userAttributes']
+    user_display_name  = user['name']
+    user_email         = user['email']
+    user_handle        = user['preferred_username']
+    user_cognito_id    = user['sub']
+    
+    sql = f"""
+      INSERT INTO public.users(
+        display_name, 
+        handle, 
+        cognito_user_id)
+      VALUES (%s, %s, %s, %s)
+    """
     try:
-        conn = psycopg2.connect(
-            host=(os.getenv('PG_HOSTNAME')),
-            database=(os.getenv('PG_DATABASE')),
-            user=(os.getenv('PG_USERNAME')),
-            password=(os.getenv('PG_SECRET'))
-        )
+        conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
         cur = conn.cursor()
-        cur.execute("INSERT INTO users (display_name, handle, cognito_user_id) VALUES(%s, %s, %s)", (user['name'], user['email'], user['sub']))
+        cur.execute(sql)
         conn.commit() 
 
     except (Exception, psycopg2.DatabaseError) as error:
