7a63c4ea66884660828532148e6902e142862cdb
Olley <olga.e.timofeeva@gmail.com>
2023-03-26 03:23:36 +0000

    adds sql template for create activity

---
+++ backend-flask/db/sql/create_activity.sql
@@ -0,0 +1,9 @@
+INSERT INTO ( user_uuid, message, expires_at)
+VALUES (
+    (SELECT uuid 
+    from public.users 
+    WHERE users.handle = %(handle)s 
+    LIMIT 1),
+    %(message)s,
+    %(expires_at)s
+) RETURNING user_uuid
