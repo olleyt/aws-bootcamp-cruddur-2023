07e9f3c2de4475e38f56c99e10975405bda9bf6f
Olley <32430092+olleyt@users.noreply.github.com>
2023-03-27 01:21:02 +0000

    makes explicit sql templates for activities

---
+++ backend-flask/db/sql/activity/object.sql
@@ -0,0 +1,11 @@
+SELECT 
+  activities.uuid,
+  users.display_name,
+  users.handle,
+  activitiws.message,
+  activities.created_at,
+  activities.expires_at
+FROM public.activities activities
+INNER JOIN public.users on users.uuid = activities.user_uuid
+WHERE
+  activities.uuid = %(uuid)s
