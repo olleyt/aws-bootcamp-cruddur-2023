0009d03e9a36807d850ac069f7630f5d5aca0acf
Olley <olga.e.timofeeva@gmail.com>
2023-04-03 02:53:43 +0000

    adds lib imports and corrects load_template call

--- backend-flask/services/message_groups.py
+++ backend-flask/services/message_groups.py
@@ -1,4 +1,7 @@
 from datetime import datetime, timedelta, timezone
+from lib.ddb import Ddb
+from lib.db import db
+
 class MessageGroups:
   def run(cognito_user_id):
     model = {
@@ -6,7 +9,7 @@ class MessageGroups:
       'data': None
     }
 
-    sql = db.template('users','uuid_from_cognito_user_id')
+    sql = db.load_template('users','uuid_from_cognito_user_id')
     my_user_uuid = db.query_value(sql,{
       'cognito_user_id': cognito_user_id
     })
