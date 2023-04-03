facda9c12674285085ec278b5ffa19c23377dbd6
Olley <olga.e.timofeeva@gmail.com>
2023-04-03 06:11:01 +0000

    sets connection_url to CONNECTION_URL

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -15,7 +15,7 @@ class Db():
         self.init_pool()
 
     def init_pool(self):
-        connection_url = os.getenv("PROD_CONNECTION_URL")
+        connection_url = os.getenv("CONNECTION_URL")
         self.pool = ConnectionPool(connection_url)
 
     def print_sql(self, title, sql, params):
