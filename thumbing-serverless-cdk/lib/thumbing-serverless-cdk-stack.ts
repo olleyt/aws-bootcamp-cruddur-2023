5d141a51dc2ead37ff435ec2cbb447a49d98a487
Olley <olga.e.timofeeva@gmail.com>
2023-04-20 03:59:57 +0000

    implements lambda and s3 bucket CDK stack

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -1,7 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda'
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+//load env variables
+const dotenv = require('dotenv')
+dotenv.config()
+
 
 export class ThumbingServerlessCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,7 +14,12 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
 
     // The code that defines your stack goes here
     const bucketName: string = process.env.THUMBING_BUCKET_NAME as string;
-    const bucket = this.createBucket(bucketName) 
+    const functionPath: string =  process.env.THUMBING_FUNCTION_PATH as string;
+    const folderInput: string = process.env.THUMBING_S3_FOLDER_INPUT as string;
+    const folderOutput: string = process.env.THUMBING_S3_FOLDER_OUTPUT as string;
+
+    const bucket = this.createBucket(bucketName);
+    const lambda = this.createlambda(functionPath, bucketName, folderInput, folderOutput) 
   }
 
   createBucket(bucketName: string):s3.IBucket {
@@ -18,5 +28,19 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
     return bucket;
+  }
+
+  createlambda(functionPath: string, bucketName: string, folderInput:string, folderOutput:string): lambda.IFunction{
+    const lambdaFunction = new lambda.Function(this, 'ThumbLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(functionPath),
+      environment: {
+        DEST_BUCKET_NAME: bucketName,
+        FOLDER_INPUT: folderInput,
+        FOLDER_OUTPUT: folderOutput
+      }
+    });
+    return lambdaFunction;
   }
 }
