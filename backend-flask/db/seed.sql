c62e048bdc79e39bae3835ce43c36b179abaa1d6
Olley <olga.e.timofeeva@gmail.com>
2023-04-25 01:01:13 +0000

    fixes mock data but masked for this commit

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,14 +1,14 @@
 -- this file was manually created
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
-  ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
+  ('Olley T', 'g@gmail.com', 'olleyt' ,'MOCK'),
+  ('Alter Ego', 'o..rt@gmail.com', 'bestie' ,'MOCK'),
+  ('Londo Mollari','ol..va@gmail.com' ,'londomollari' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'olleyt' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
